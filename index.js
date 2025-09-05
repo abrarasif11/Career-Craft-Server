945371004f3c5dd38a83015de8ce90b9ef984572
abrarasif11 <faasif929@gmail.com>
2025-09-05 18:30:30 +0600

    server running successful

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const port = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+app.get('/', (req,res) => {
+    res.send("Career Craft server is running");
+});
+
+app.listen(port, () => {
+    console.log(`Career Craft Server is running on PORT:${port}`)
+});
