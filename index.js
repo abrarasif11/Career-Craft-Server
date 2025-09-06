4252d300eabc98cbe2f24afe38e98c26539cadec
abrarasif11 <faasif929@gmail.com>
2025-09-06 14:54:24 +0600

    worst way to get data

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 7000;
 
-// Middleware 
+// Middleware
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,40 +29,57 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-   //Job related API
-   const jobsCollection = client.db('CareerCraft').collection('jobs');
-   const jobApplyCollection = client.db('CareerCraft').collection('job-application');
-   app.get('/jobs', async(req, res) =>{
-    const cursor = jobsCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-   })
- 
-    app.get('/jobs/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await jobsCollection.findOne(query);
-        res.send(result)
-    })
-   
-// job-application API
-// get all data , get on data , get some data [0,1,many]
+    //Job related API
+    const jobsCollection = client.db("CareerCraft").collection("jobs");
+    const jobApplyCollection = client
+      .db("CareerCraft")
+      .collection("job-application");
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-   app.get('/job-application', async(req,res) => {
-    const email = req.query.email;
-    const query = { applicant_email: email }
-    const result = await jobApplyCollection.find(query).toArray();
-    res.send(result);
-   })
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
 
-   app.post('/job-application', async (req, res) => {
-    const application = req.body;
-    const result = await jobApplyCollection.insertOne(application)
-    res.send(result)
-  })
+    // job-application API
+    // get all data , get on data , get some data [0,1,many]
 
+    app.get("/job-application", async (req, res) => {
+      const email = req.query.email;
+      const query = { applicant_email: email };
+      const result = await jobApplyCollection.find(query).toArray();
+
+      // Worst Way to get data
+      for (const application of result){
+        console.log(application.job_id)
+        const query1 = { _id: new ObjectId(application.job_id) }
+        const job = await jobsCollection.findOne(query1)
+        if(job){
+          application.title = job.title;
+          application.company = job.company
+          application.company_logo = job.company_logo
+          
+        }
+      }
+
+      res.send(result);
+    });
+
+    app.post("/job-application", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplyCollection.insertOne(application);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -70,11 +87,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req,res) => {
-    res.send("Career Craft server is running");
+app.get("/", (req, res) => {
+  res.send("Career Craft server is running");
 });
 
 app.listen(port, () => {
-    console.log(`Career Craft Server is running on PORT:${port}`)
+  console.log(`Career Craft Server is running on PORT:${port}`);
 });
