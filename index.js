e75663cedc9fbc5f8096b1ff4f64d6bb5769e645
abrarasif11 <faasif929@gmail.com>
2025-09-09 22:51:16 +0600

    cors origin

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ require("dotenv").config();
 const port = process.env.PORT || 7000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173/'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser())
 
