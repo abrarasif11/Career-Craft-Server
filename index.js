b838b453b9702396f45d8c4e18ccf1da432d1a4c
abrarasif11 <faasif929@gmail.com>
2025-09-10 18:45:50 +0600

    axios error

--- index.js
+++ index.js
@@ -24,20 +24,19 @@ const logger = (req, res, next) => {
 };
 
 const verifyToken = (req, res, next) => {
-  console.log("Inside the verify token", req.cookies);
   const token = req?.cookies?.token;
   if (!token) {
-    return res.status(401).send({ message: "Unauthorized Access" });
+      return res.status(401).send({ message: 'unAuthorized access' })
   }
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized Access" });
-    }
-    req.user = decoded;
-    next();
-  });
- 
-};
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+      }
+      req.user = decoded;
+      next();
+  })
+}
 //Mongo Code
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhdpi0m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -121,14 +120,14 @@ async function run() {
     });
 
     // ================= Job Application API =================
-    app.get("/job-application", verifyToken, async (req, res) => {
+    app.get("/job-application", async (req, res) => {
       try {
         const email = req.query.email;
         const query = { applicant_email: email };
        
-        if(req.user.email !== req.query.email){
-          return res.status(403).send({ message : 'Forbidden Access'})
-        }
+        // if(req.user.email !== req.query.email){
+        //   return res.status(403).send({ message : 'Forbidden Access'})
+        // }
 
         const result = await jobApplyCollection.find(query).toArray();
 
